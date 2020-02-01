0038d7f1a7aba41a6455bf78f1f3df9f210cc2e5
Dahri Fati <mohaadhr@MacBook-Air-de-Dahri.local>
2020-02-01 21:38:30 +0100

    Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,30 @@
-import React, { Fragment } from "recat";
+import React, { Fragment } from "react";
+import Logo from "../img/logo.png";
 
 const Nav = () => {
     return (
         <Fragment>
 
+            <div className="row">
+                <div className="col-2">
+                    <img className="logo" src={Logo} alt="" />
+                </div>
+
+                <ul className="liens col-10 nav justify-content-end">
+                    <li className="nav-item">
+                        <a className="nav-link active disabled" href="#">Active</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
         </Fragment>
     );
 }
