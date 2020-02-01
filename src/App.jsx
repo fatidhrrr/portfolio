79c132ebbe26943530e3fab90e03ca81a8c29d83
Dahri Fati <mohaadhr@MacBook-Air-de-Dahri.local>
2020-02-01 20:48:12 +0100

    début

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./css/style.css";
+import Nav from "./components/Nav";
+
+const App = () => {
+    return (
+        <Fragment>
+            <Nav/>
+        </Fragment>
+    );
+}
+
+
+export default App;
